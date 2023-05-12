64dd0ad7225493a723736e170d9809dcc5de660e
Shuyang12138 <50036212+Shuyang12138@users.noreply.github.com>
2023-05-11 19:33:33 -0500

    Update test_ner.sh

--- code/test_ner.sh
+++ code/test_ner.sh
@@ -1,19 +1,19 @@
 #--model_name_or_path dmis-lab/biobert-base-cased-v1.1 \
-export SAVE_DIR=./output_SL_chem
+export SAVE_DIR=./output_CP
 export DATA_DIR=../datasets/NER
 
 export MAX_LENGTH=192
 export BATCH_SIZE=3
 export NUM_EPOCHS=10
 export SAVE_STEPS=1000
-export ENTITY=BC5CDR-chem #NCBI-disease/labeled_part
+export ENTITY=NCBI-disease/labeled_part
 export SEED=1
 
 
 python run_ner.py \
     --data_dir ${DATA_DIR}/${ENTITY}/ \
     --labels ${DATA_DIR}/${ENTITY}/labels.txt \
-    --model_name_or_path output_SL/ \
+    --model_name_or_path output_CP/ \
     --tokenizer_name dmis-lab/biobert-base-cased-v1.1\
     --output_dir ${SAVE_DIR}/${ENTITY} \
     --max_seq_length ${MAX_LENGTH} \
