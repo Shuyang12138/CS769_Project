19deb7f4f69730e1e1fab6eae7c7112c22c64a89
Shuyang12138 <50036212+Shuyang12138@users.noreply.github.com>
2023-05-11 17:32:26 -0700

    Add files via upload

--- code/test_ner.sh
+++ code/test_ner.sh
@@ -1,18 +1,20 @@
-export SAVE_DIR=./output_crf
+#--model_name_or_path dmis-lab/biobert-base-cased-v1.1 \
+export SAVE_DIR=./output_SL_chem
 export DATA_DIR=../datasets/NER
 
 export MAX_LENGTH=192
-export BATCH_SIZE=32
-export NUM_EPOCHS=30
+export BATCH_SIZE=3
+export NUM_EPOCHS=10
 export SAVE_STEPS=1000
-export ENTITY=NCBI-disease
+export ENTITY=BC5CDR-chem #NCBI-disease/labeled_part
 export SEED=1
 
 
 python run_ner.py \
     --data_dir ${DATA_DIR}/${ENTITY}/ \
     --labels ${DATA_DIR}/${ENTITY}/labels.txt \
-    --model_name_or_path dmis-lab/biobert-base-cased-v1.1 \
+    --model_name_or_path output_SL/ \
+    --tokenizer_name dmis-lab/biobert-base-cased-v1.1\
     --output_dir ${SAVE_DIR}/${ENTITY} \
     --max_seq_length ${MAX_LENGTH} \
     --num_train_epochs ${NUM_EPOCHS} \
@@ -20,7 +22,7 @@ python run_ner.py \
     --save_steps ${SAVE_STEPS} \
     --seed ${SEED} \
     --do_train \
-    --do_eval \
     --do_predict \
     --overwrite_output_dir \
     --auto_find_batch_size 
+    --do_eval \
